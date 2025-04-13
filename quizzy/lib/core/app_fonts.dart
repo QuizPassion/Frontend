ae4a195e6b81a3b99ad2f47dcfb3f53f2377a189
letienne <etienne.lemee@ynov.com>
2025-04-13 17:55:24 +0200

    feat: added fonts, color variables, logos

---
+++ quizzy/lib/core/app_fonts.dart
@@ -0,0 +1,23 @@
+class AppFonts {
+  static const String montserrat = 'Montserrat';
+  static const String lato = 'Lato';
+}
+
+/* 
+  Text(
+    'Title',
+    style: const TextStyle(
+      fontFamily: AppFonts.montserrat,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    ),
+  ),
+
+  Text(
+    'Regular text here',
+    style: const TextStyle(
+      fontFamily: AppFonts.lato,
+      fontSize: 16,
+    ),
+  ),
+*/
