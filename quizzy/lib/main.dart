d31c1d5e2501e940359261ca05c88d671fe643c3
letienne <etienne.lemee@ynov.com>
2025-04-17 22:43:38 +0200

    feat: new routes

--- quizzy/lib/main.dart
+++ quizzy/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:quizzy/data/viewmodel/auth_view_model.dart';
+import 'package:quizzy/views/create-quiz/all_quiz.dart';
+import 'package:quizzy/views/create-quiz/create_quiz.dart';
+import 'package:quizzy/views/create-quiz/create_quiz_questions.dart';
 import 'package:quizzy/views/home/home.dart';
+import 'package:quizzy/views/in-game/created_game_lobby.dart';
+import 'package:quizzy/views/in-game/joined_game_lobby.dart';
+import 'package:quizzy/views/parameters/parameters.dart';
+import 'package:quizzy/views/score/score.dart';
 import 'package:quizzy/views/welcome/welcome.dart';
 import 'package:quizzy/views/login/login.dart';
 import 'package:quizzy/views/signup/signup.dart';
@@ -35,6 +42,15 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const SignUpPage(),
         '/home': (context) => const HomePage(),
+        '/welcome': (context) => const WelcomePage(),
+
+        '/allQuiz': (context) => const AllQuizPage(),
+        '/createQuiz': (context) => const CreateQuizPage(),
+        '/createQuizQuestions': (context) => const CreateQuizQuestionsPage(),
+        '/score': (context) => const ScorePage(),
+        '/parameters': (context) => const ParametersPage(),
+        '/joinedGameLobby': (context) => const JoinedGameLobbyPage(),
+        '/createdGameLobby': (context) => const CreatedGameLobbyPage(),
       },
     );
   }
