650713c10be6860fbd99ee02566e09a0c5d7a98a
pierrealexi <favrilpierrealexis@gmail.com>
2025-04-17 15:53:40 +0200

    fix: url Login

--- quizzy/lib/data/network/config.dart
+++ quizzy/lib/data/network/config.dart
@@ -1,5 +1,5 @@
 class Config{
   static const String baseUrl = "http://10.0.2.2:8080/api/v1";
-  static const String loginEndPoint = "$baseUrl/login";
+  static const String loginEndPoint = "$baseUrl/Login";
   static const String signupEndpoint = "$baseUrl/NewUser/register";
 }
