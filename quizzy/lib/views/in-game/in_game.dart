b6d92d01a37fb1582c4b5527ca914396e48b8c87
letienne <etienne.lemee@ynov.com>
2025-04-23 13:56:18 +0200

    feat: ingame and game loading pages

---
+++ quizzy/lib/views/in-game/in_game.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../../core/app_colors.dart';
+import '../../core/app_fonts.dart';
+import '../../core/widgets/app_bar.dart';
+import '../../core/widgets/background_decoration.dart';
+import '../../core/widgets/nav_bar.dart';
+import 'widgets/answer_question_card.dart'; // ⬅️ Import new widget
+
+class InGame extends StatelessWidget {
+  const InGame({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const QuizzyAppBar(),
+      backgroundColor: AppColors.anthraciteBlack,
+      body: Stack(
+        children: [
+          const BackgroundDecoration(child: SizedBox.shrink()),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Quiz sur les jedi dans Star Wars',
+                  style: TextStyle(
+                    fontFamily: AppFonts.montserrat,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.lightGrey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  'Question 1/15',
+                  style: TextStyle(
+                    fontFamily: AppFonts.montserrat,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.lightGrey,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '1/20 joueurs ont répondu',
+                  style: TextStyle(
+                    fontFamily: AppFonts.lato,
+                    fontSize: 14,
+                    color: AppColors.lightGrey,
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // question card
+                const AnswerQuestionCard(),
+                
+                // quit btn
+                
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: QuizzyNavBar(
+        currentIndex: 4,
+        onTap: (index) {},
+      ),
+    );
+  }
+}
